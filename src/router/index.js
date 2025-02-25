c3aa1278c6bc5e33559e2fdbde43acbabbbdcb6a
David Nyaga <105038549+Nyagatech@users.noreply.github.com>
2025-02-25 09:31:54 +0300

    we are able to get all the movies from the api

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import listingShows from '@/components/listingShows.vue'
  
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'Home',
+      component: listingShows
      
     },
     {
+      
       
     },
   ],
